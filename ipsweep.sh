c13710e18a48f1184fc3df9d94af24e5623e89e9
Aryan Tiwary <69082867+aryan7tiwary@users.noreply.github.com>
2021-06-19 23:48:05 +0530

    Update ipsweep.sh

--- ipsweep.sh
+++ ipsweep.sh
@@ -3,8 +3,10 @@
 echo "Enter first three point of IP address. Eg: 192.168.1"
 read input
 echo
+echo "Maybe wait for a minute if you want full scan."
 echo "Available IP addresses:"
 for ip in {1..254};
 do
 	(ping -c 1 $input.$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" &)
+	sleep 0.2
 done
